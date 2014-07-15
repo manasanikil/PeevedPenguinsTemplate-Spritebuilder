a88eadb6c95f39bbc02b3ac9ee9fc3c192fd33bb
manasanikil <srikantlakshminarayan@srikants-mbp.home>
2014-07-14 17:19:00 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Penguin.h
@@ -0,0 +1,13 @@
+//
+//  Penguin.h
+//  PeevedPenguins
+//
+//  Created by ManasaNikil on 7/14/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Penguin : CCSprite
+
+@end
---
+++ PeevedPenguins.spritebuilder/Source/Seal.h
@@ -0,0 +1,13 @@
+//
+//  Seal.h
+//  PeevedPenguins
+//
+//  Created by ManasaNikil on 7/14/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Seal : CCSprite
+
+@end
